1f5e5facf6dda7da9ac346f4e2d62165c278cf5f
Byrch Baker <byrchbaker@gmail.com>
2023-10-10 23:44:20 -0500

    windoes + linox :)

--- src/toast.rs
+++ src/toast.rs
@@ -2,13 +2,18 @@ use notify_rust::Notification;
 use rodio::{source::Source, Decoder, OutputStream};
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
+use winrt_notification::{Duration, Toast};
+use winrt_toast::register;
 
+#[cfg(target_os = "linux")]
 pub fn toast(category: &str) {
     match category {
         "gain" => {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
             // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(File::open("/opt/sentinel/media/sound/gain.wav").unwrap());
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\gain.wav").unwrap());
             // Decode that sound file into a source
             let source = Decoder::new(file).unwrap();
             // Play the sound directly on the device
@@ -17,7 +22,8 @@ pub fn toast(category: &str) {
             Notification::new()
                 .summary("Points Earned")
                 .body("You Earned Points!")
-                .icon("/opt/sentinel/media/logo.ico")
+                .icon("C:\\opt\\sentinel\\media\\logo.ico")
+                .appname("Sentinel")
                 .show()
                 .unwrap();
 
@@ -26,7 +32,8 @@ pub fn toast(category: &str) {
         "lost" => {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
             // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(File::open("/opt/sentinel/media/sound/alarm.wav").unwrap());
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\alarm.wav").unwrap());
             // Decode that sound file into a source
             let source = Decoder::new(file).unwrap();
             // Play the sound directly on the device
@@ -35,7 +42,7 @@ pub fn toast(category: &str) {
             Notification::new()
                 .summary("Points Lost")
                 .body("You Lost Points!")
-                .icon("/opt/sentinel/media/logo.ico")
+                .icon("C:\\opt\\sentinel\\media\\logo.ico")
                 .show()
                 .unwrap();
 
@@ -44,7 +51,8 @@ pub fn toast(category: &str) {
         "siren" => {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
             // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(File::open("/opt/sentinel/media/sound/siren.wav").unwrap());
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\siren.wav").unwrap());
             // Decode that sound file into a source
             let source = Decoder::new(file).unwrap();
             // Play the sound directly on the device
@@ -53,7 +61,7 @@ pub fn toast(category: &str) {
             Notification::new()
                 .summary("Engine Status")
                 .body("Please enter your unique identifier!")
-                .icon("/opt/sentinel/media/logo.ico")
+                .icon("C:\\opt\\sentinel\\media\\logo.ico")
                 .show()
                 .unwrap();
 
@@ -62,7 +70,8 @@ pub fn toast(category: &str) {
         "gong" => {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
             // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(File::open("/opt/sentinel/media/sound/gong.wav").unwrap());
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\gong.wav").unwrap());
             // Decode that sound file into a source
             let source = Decoder::new(file).unwrap();
             // Play the sound directly on the device
@@ -71,7 +80,7 @@ pub fn toast(category: &str) {
             Notification::new()
                 .summary("Time Alert")
                 .body("Your competition time is running out!")
-                .icon("/opt/sentinel/media/logo.ico")
+                .icon("C:\\opt\\sentinel\\media\\logo.ico")
                 .show()
                 .unwrap();
 
@@ -80,7 +89,8 @@ pub fn toast(category: &str) {
         "horn" => {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
             // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(File::open("/opt/sentinel/media/sound/horn.wav").unwrap());
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\horn.wav").unwrap());
             // Decode that sound file into a source
             let source = Decoder::new(file).unwrap();
             // Play the sound directly on the device
@@ -89,7 +99,169 @@ pub fn toast(category: &str) {
             Notification::new()
                 .summary("Time Alert")
                 .body("Your team has exceeded the competition time.")
-                .icon("/opt/sentinel/media/logo.ico")
+                .icon("C:\\opt\\sentinel\\media\\logo.ico")
+                .show()
+                .unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+        _ => println!("Input does not equal any value"),
+    }
+}
+
+#[cfg(target_os = "windows")]
+pub fn toast(category: &str) {
+    match category {
+        "gain" => {
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\gain.wav").unwrap());
+            // Decode that sound file into a source
+            let source = Decoder::new(file).unwrap();
+            // Play the sound directly on the device
+            let _ = stream_handle.play_raw(source.convert_samples());
+
+            let _ = register(
+                "SENTINEL",
+                "Sentinel",
+                Some(Path::new("C:\\opt\\sentinel\\media\\logo.ico")),
+            );
+
+            Toast::new("SENTINEL")
+                .title("Points Earned")
+                .text1("You Earned Points!")
+                .sound(None)
+                .duration(Duration::Short)
+                .icon(
+                    Path::new("C:\\opt\\sentinel\\media\\logo.ico"),
+                    winrt_notification::IconCrop::Circular,
+                    "Sentinel",
+                )
+                .show()
+                .unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+        "lost" => {
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\alarm.wav").unwrap());
+            // Decode that sound file into a source
+            let source = Decoder::new(file).unwrap();
+            // Play the sound directly on the device
+            let _ = stream_handle.play_raw(source.convert_samples());
+
+            let _ = register(
+                "SENTINEL",
+                "Sentinel",
+                Some(Path::new("C:\\opt\\sentinel\\media\\logo.ico")),
+            );
+
+            Toast::new("SENTINEL")
+                .title("Points Lost")
+                .text1("You Lost Points!")
+                .sound(None)
+                .duration(Duration::Short)
+                .icon(
+                    Path::new("C:\\opt\\sentinel\\media\\logo.ico"),
+                    winrt_notification::IconCrop::Circular,
+                    "Sentinel",
+                )
+                .show()
+                .unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+        "siren" => {
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\siren.wav").unwrap());
+            // Decode that sound file into a source
+            let source = Decoder::new(file).unwrap();
+            // Play the sound directly on the device
+            let _ = stream_handle.play_raw(source.convert_samples());
+
+            let _ = register(
+                "SENTINEL",
+                "Sentinel",
+                Some(Path::new("C:\\opt\\sentinel\\media\\logo.ico")),
+            );
+
+            Toast::new("SENTINEL")
+                .title("Engine Status")
+                .text1("Please enter your unique identifier!")
+                .sound(None)
+                .duration(Duration::Short)
+                .icon(
+                    Path::new("C:\\opt\\sentinel\\media\\logo.ico"),
+                    winrt_notification::IconCrop::Circular,
+                    "Sentinel",
+                )
+                .show()
+                .unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+        "gong" => {
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\gong.wav").unwrap());
+            // Decode that sound file into a source
+            let source = Decoder::new(file).unwrap();
+            // Play the sound directly on the device
+            let _ = stream_handle.play_raw(source.convert_samples());
+
+            let _ = register(
+                "SENTINEL",
+                "Sentinel",
+                Some(Path::new("C:\\opt\\sentinel\\media\\logo.ico")),
+            );
+
+            Toast::new("SENTINEL")
+                .title("Time Alert")
+                .text1("Your competition time is running out!")
+                .sound(None)
+                .duration(Duration::Short)
+                .icon(
+                    Path::new("C:\\opt\\sentinel\\media\\logo.ico"),
+                    winrt_notification::IconCrop::Circular,
+                    "Sentinel",
+                )
+                .show()
+                .unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+        "horn" => {
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file =
+                BufReader::new(File::open("C:\\opt\\sentinel\\media\\sound\\horn.wav").unwrap());
+            // Decode that sound file into a source
+            let source = Decoder::new(file).unwrap();
+            // Play the sound directly on the device
+            let _ = stream_handle.play_raw(source.convert_samples());
+
+            let _ = register(
+                "SENTINEL",
+                "Sentinel",
+                Some(Path::new("C:\\opt\\sentinel\\media\\logo.ico")),
+            );
+
+            Toast::new("SENTINEL")
+                .title("Time Alert")
+                .text1("Your team has exceeded the competition time.")
+                .sound(None)
+                .duration(Duration::Short)
+                .icon(
+                    Path::new("C:\\opt\\sentinel\\media\\logo.ico"),
+                    winrt_notification::IconCrop::Circular,
+                    "Sentinel",
+                )
                 .show()
                 .unwrap();
 
